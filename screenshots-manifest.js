25661243fb7e4b380e60e506ccc865c2635bd028
dreaiith <andreafaith.cunanan@neu.edu.ph>
2026-07-08 18:07:27 +0800

    first commit

---
+++ screenshots-manifest.js
@@ -0,0 +1,28 @@
+/*
+  SCREENSHOT MANIFEST
+  --------------------
+  Drop your screenshot images into the "screenshots" folder next to this file,
+  then add a line per lab below, matching the lab's number (e.g. "16.7")
+  to one image, or an array of images if you have more than one for that lab.
+
+  Example:
+  const SCREENSHOTS = {
+    "16.7": "screenshots/16-7.png",
+    "19.1": ["screenshots/19-1-a.png", "screenshots/19-1-b.png"]
+  };
+
+  Any lab listed here will automatically:
+   - get pulled into the "Featured" section on the homepage
+   - show its screenshot(s) at the top of its write-up when opened
+
+  Add more lines any time you upload more screenshots.
+*/
+const SCREENSHOTS = {
+  "19.1": ["screenshots/19-1-a.png", "screenshots/19-1-b.png", "screenshots/19-1-c.png"],
+  "13.9": "screenshots/13-9-a.png",
+  "13.4": "screenshots/13-4-a.png",
+  "8.2": "screenshots/8-2-a.png",
+  "4.1": ["screenshots/4-1-a.png", "screenshots/4-1-b.png", "screenshots/4-1-c.png"],
+  "3.1": ["screenshots/3-1-a.png", "screenshots/3-1-b.png"],
+  "20.1": "screenshots/20-1-a.png"
+};
